21915d62876abb2130cf07e6f171e74d2138d48e
Pammikhatun936 <pammikhatoon28@gmail.com>
2025-02-12 17:17:24 +0530

    Add files via upload

---
+++ CompoundInterest.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class CompoundInterest {
+        public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+       System.out.print("Enter The Value Of principal: ");
+       double p =sc.nextDouble();
+       System.out.print("Enter The Value Of rate: ");
+      double r =sc.nextDouble();
+       System.out.print("Enter The Value Of time: ");
+      double t =sc.nextDouble();
+     double a = p*(Math.pow(1+r/100, t));
+      double cp=a-p;
+      System.out.println("Compound Interest Is :"+ cp);
+      sc.close();
+}
+}
